d39cca839d7857e995b2d4a2312e1dd8706f2d85
sakhmed99 <105075171+sakhmed99@users.noreply.github.com>
2022-08-26 10:55:06 +0200

    Base project

---
+++ src/test/java/com/sinensia/netflixgroup5/NetflixGroup5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.netflixgroup5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixGroup5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
